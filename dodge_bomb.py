71edc25975ffeea0c8cbe2f4418e122cc0847e6b
fujisawa <c0a23131d8@edu.teu.ac.jp>
2024-10-29 18:06:40 +0900

    初期状態２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -27,7 +27,66 @@ def check_bound(obj_rct: pg.rect) -> tuple[bool, bool]:
         tate = False
     
     return yoko,tate
-    
+
+class Bird:
+    """
+    ゲームキャラクター（こうかとん）に関するクラス
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0),
+    }
+    img0 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+    imgs = {  # 0度から反時計回りに定義
+        (+5, 0): img,  # 右
+        (+5, -5): pg.transform.rotozoom(img, 45, 0.9),  # 右上
+        (0, -5): pg.transform.rotozoom(img, 90, 0.9),  # 上
+        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9),  # 左上
+        (-5, 0): img0,  # 左
+        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9),  # 左下
+        (0, +5): pg.transform.rotozoom(img, -90, 0.9),  # 下
+        (+5, +5): pg.transform.rotozoom(img, -45, 0.9),  # 右下
+    }
+
+    def __init__(self, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数 xy：こうかとん画像の初期位置座標タプル
+        """
+        self.img = __class__.imgs[(+5, 0)]
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = xy
+
+    def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        screen.blit(self.img, self.rct)
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        self.rct.move_ip(sum_mv)
+        if check_bound(self.rct) != (True, True):
+            self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = __class__.imgs[tuple(sum_mv)]
+        screen.blit(self.img, self.rct)
+
 def gameover(screen):
     """
     ゲームオーバー時の処理
@@ -58,39 +117,63 @@ def enemy(num,screen: pg.surface):
         en_rct1.centerx = 150
         en_rct1.centery = HEIGHT/2
         screen.blit(en_img1,en_rct1)
-        
+        stage1()
+
     elif num == 2:
         en_img2 = pg.transform.rotozoom(pg.image.load("fig/en1.png"),0,0.4)
         en_rct2 = en_img2.get_rect()
         en_rct2.centerx = WIDTH-150
         en_rct2.centery = HEIGHT/2
         screen.blit(en_img2,en_rct2)
+        stage2()
 
     elif num == 3:
-        en_img5 = pg.transform.rotozoom(pg.image.load("fig/en5.png"),0,1)
-        en_img6 = pg.transform.rotozoom(pg.image.load("fig/en6.png"),0,1)
-        
+        en_img3 = pg.transform.rotozoom(pg.image.load("fig/en5.png"),0,1)
+        en_img4 = pg.transform.rotozoom(pg.image.load("fig/en6.png"),0,1)
+        en_rct3 = en_img3.get_rect()
+        en_rct3.centerx = 150
+        en_rct3.centery = HEIGHT/2
+        en_rct4 = en_img4.get_rect()
+        en_rct4.centerx = WIDTH-150
+        en_rct4.centery = HEIGHT/2
+        screen.blit(en_img3,en_rct3)
+        screen.blit(en_img4,en_rct4)
+        stage3()
+
+    elif num == 4:
+        en_img5 = pg.transform.rotozoom(pg.image.load("fig/en7.png"),0,0.6)
         en_rct5 = en_img5.get_rect()
         en_rct5.centerx = 150
         en_rct5.centery = HEIGHT/2
-        en_rct6 = en_img6.get_rect()
-        en_rct6.centerx = WIDTH-150
-        en_rct6.centery = HEIGHT/2
         screen.blit(en_img5,en_rct5)
-        screen.blit(en_img6,en_rct6)
-   
+        stageEX()
 
+
+def stage1():
+    return 0
+
+def stage2():
+    return 0
+
+def stage3():
+    return 0
+
+def stageEX():
+    return 0
+
+def timescore():
+    return 0
+
+def skill():
+    return 0
 
 def main():
     pg.display.set_caption("避けろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.transform.rotozoom(pg.image.load("fig/bg.png"),0,1.9)    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = WIDTH/2, HEIGHT/2
-    stage = 3
-
-
+    bird = Bird([WIDTH/2, HEIGHT/2])
+    stage = 1
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -109,15 +192,11 @@ def main():
                 sum_mv[0] += tpl[0] #横方向
                 sum_mv[1] += tpl[1] #縦方向
             
-        
-        kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True,True):
-            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
 
-        screen.blit(kk_img, kk_rct)
+        bird.update(key_lst, screen)
         enemy(stage,screen)
-        
-            
+        timescore()
+        skill()
         pg.display.update()
         tmr += 1
         clock.tick(50)
